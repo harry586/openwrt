4186f93f1703c0ca1143b8efc362951b4d9eed74
harry586 <38090655+harry586@users.noreply.github.com>
2026-02-07 08:34:54 +0800

    Update support.sh

--- support.sh
+++ support.sh
@@ -6,30 +6,10 @@
 # 设备配置映射表（关联数组）
 declare -A DEVICES
 
-# ASUS 华硕系列
-DEVICES["ac42u"]="ipq40xx generic asus_rt-ac42u"
-DEVICES["acrh17"]="ipq40xx generic asus_rt-ac42u"
-
-# 小米/红米系列
-DEVICES["ax6s"]="mediatek mt7622 xiaomi_redmi-router-ax6s"
-DEVICES["ax6"]="qualcommax ipq807x xiaomi_ax3600"
-DEVICES["wr30u"]="mediatek mt7981 xiaomi_wr30u"
-DEVICES["miwifi-mini"]="ramips mt7620 xiaomi_miwifi-mini"
-DEVICES["miwifi-3g"]="ramips mt7621 xiaomi_mi-router-3g"
-DEVICES["redmi-ac2100"]="ramips mt7621 xiaomi_redmi-router-ac2100"
-
-# 360 安全路由器
-DEVICES["360t7"]="mediatek mt7981 360_t7"
-
-# 斐讯系列
-DEVICES["k2p"]="ramips mt7621 phicomm_k2p"
-DEVICES["k2"]="ramips mt7620 phicomm_k2"
-
-# 极路由系列
-DEVICES["hc5962"]="ramips mt7621 hiwifi_hc5962"
-
-# 新添加的设备可以在这里扩展
-# DEVICES["new_device"]="target subtarget device_model"
+# 支持的设备列表（仅3个设备）
+DEVICES["ac42u"]="ipq40xx generic"
+DEVICES["cmcc_rax3000m"]="mediatek mt7981"
+DEVICES["netgear_3800"]="ath79 generic"
 
 # 获取设备配置函数
 get_device_config() {
